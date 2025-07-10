b3ac58d8a14bffd447a9524a63347bb9726915da
Ana Valeska Santos <avaleska2908@gmail.com>
2025-07-10 09:27:14 -0300

    feat: Add create question, create room and get room questions.

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,6 +1,6 @@
-import { reset, seed } from 'drizzle-seed';
-import { db, sql } from './connection.ts';
-import { schema } from './schema/index.ts';
+import { reset, seed } from "drizzle-seed";
+import { db, sql } from "./connection.ts";
+import { schema } from "./schema/index.ts";
 
 await reset(db, schema);
 await seed(db, schema).refine((faker) => {
@@ -12,9 +12,12 @@ await seed(db, schema).refine((faker) => {
         description: faker.loremIpsum(),
       },
     },
+    questions: {
+      count: 20,
+    },
   };
 });
 
 await sql.end();
 // biome-ignore lint/suspicious/noConsole: only used in dev
-console.log('Database seeded');
+console.log("Database seeded");
